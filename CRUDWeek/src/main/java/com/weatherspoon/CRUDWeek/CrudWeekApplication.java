509afe8a5adc67d4eba8f63ea31e8742e7313a34
Corban Weatherspoon <corbanweatherspoon@Corbans-MacBook-Air.local>
2021-02-08 19:19:02 -0500

    Added Spring Project

---
+++ CRUDWeek/src/main/java/com/weatherspoon/CRUDWeek/CrudWeekApplication.java
@@ -0,0 +1,13 @@
+package com.weatherspoon.CRUDWeek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudWeekApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudWeekApplication.class, args);
+	}
+
+}
---
+++ CRUDWeek/src/test/java/com/weatherspoon/CRUDWeek/CrudWeekApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weatherspoon.CRUDWeek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudWeekApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
